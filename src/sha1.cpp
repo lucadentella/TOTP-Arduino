c6f392562c3f6bb57105ebea177714f8f6d1aa6d
mikeazo <undefinedspace@gmail.com>
2018-03-06 15:35:48 -0500

    Remove PROGMEM and the associated (memcpy_P)

--- src/sha1.cpp
+++ src/sha1.cpp
@@ -6,7 +6,7 @@
 #define SHA1_K40 0x8f1bbcdc
 #define SHA1_K60 0xca62c1d6
 
-const uint8_t sha1InitState[] PROGMEM = {
+const uint8_t sha1InitState[] = {
   0x01,0x23,0x45,0x67, // H0
   0x89,0xab,0xcd,0xef, // H1
   0xfe,0xdc,0xba,0x98, // H2
@@ -15,7 +15,7 @@ const uint8_t sha1InitState[] PROGMEM = {
 };
 
 void Sha1Class::init(void) {
-  memcpy_P(state.b,sha1InitState,HASH_LENGTH);
+  memcpy(state.b,sha1InitState,HASH_LENGTH);
   byteCount = 0;
   bufferOffset = 0;
 }
